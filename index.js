d5af421bfa67af84ae3bd74e3a928b815bc96b16
Amnuve <amnuve@outlook.it>
2024-10-28 17:31:39 +0100

    Lezione Api e Closure

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-fetch('https://jsonplaceholder.typicode.com/todos/')
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+const createTodoDiv = (todo) => {
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    input.type = "checkbox";
+    input.id = todo.id;
+    input.checked = todo.completed;
+    input.onchange = handleCheckbox; //riferimento alla funzione si scrive senza le ()
+    const label = document.createElement('label');
+    label.textContent = todo.title;
+    div.appendChild(input);
+    div.appendChild(label);
+    return div;
+}
+
+const loading = document.querySelector('#todos .loading');
+const todoDiv = document.querySelector('#todos');
+
+const todoManager = () => {
+    let state = [];
+    return {
+        set: function(newState) {
+            state = newState;
+            this.render();
+        },
+        render: function(){
+            todoDiv.innerHTML = "";
+            state.forEach(element => {
+                todoDiv.appendChild(createTodoDiv(element));
+            })
+        },
+        add: function(newElement){
+            state.unshift(newElement);
+            this.render();
+        }
+    }
+}
+const firstTodos = todoManager();
+
+fetch(baseUrl + '/todos/')
     .then(response => response.json())
     .then(todos => {
-        const loading = document.querySelector('#todos .loading');
-        const todoDiv = document.querySelector('#todos');
         loading.remove();
-        todos.forEach(todo => {
-            const div = document.createElement('div');
-            const input = document.createElement('input');
-            input.type = "checkbox";
-            input.id = todo.id;
-            input.checked = todo.completed;
-            input.onchange = handleCheckbox; //riferimento alla funzione si scrive senza le ()
-            const label = document.createElement('label');
-            label.textContent = todo.title;
-            div.appendChild(input);
-            div.appendChild(label);
-            todoDiv.appendChild(div);
-        });
+        firstTodos.set(todos);
     })
     .catch((err)=>{
     console.log(err);
@@ -24,8 +50,12 @@ fetch('https://jsonplaceholder.typicode.com/todos/')
 
 const handleCheckbox = (event) => {
     console.log(event.target.checked);
-    fetch('https://jsonplaceholder.typicode.com/todos/' + event.target.id,
-        { method: 'PUT'}
+    fetch(baseUrl + '/todos/'+ event.target.id,
+        { 
+            method: 'PATCH',
+            body: JSON.stringify({completed: event.target.checked()}),
+            headers:{ "Content-Type": "application/json" }  
+        }
     )
     .then(response => response.json())
     .then(todo => {
@@ -35,3 +65,26 @@ const handleCheckbox = (event) => {
         event.target.checked = !event.target.checked;
     })
 }
+
+const todoForm = document.querySelector("#todoForm");
+// todoForm.onsubmit = () => {} questo sovrascrive eventi precedenti
+    todoForm.addEventListener('submit', (event) => { //questo aggiunge un evento senza eliminare i precedenti => molto più versatile
+        event.preventDefault();
+        const title = event.target.children.title;
+        console.log(title.value);
+        fetch(baseUrl + '/todos/',{
+            method: 'POST',
+            body: JSON.stringify({
+                title: title.value,
+                completed: false,
+                userId: 1,
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+    .then(response => response.json())
+    .then(newTodo => { 
+        firstTodos.add(newTodo);
+    })
+})
+
+//aggiungi modifica ed elimina
